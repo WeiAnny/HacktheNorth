54f207b8b75be9698d17bed5b8121cddc79f65ec
Anny Wei <anny.wei@student.tdsb.on.ca>
2021-09-19 00:47:18 -0400

    something works

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import *
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATA']
+#app.config['SQLALCHEMY_DATA']
 
 #@app.route('/', methods = ['POST'])
 #def homepage():
@@ -16,10 +16,10 @@ def index():
 
 @app.route('/', methods = ['POST'])
 def get_stock():
-    if request.method == 'POST':
-        form = request.form #json format? ish
-        stock = form['stock'] #id in html
-        return render_template('index.html')
+    stock = request.form['stock'] #id in html
+    startdate = request.form['startdate']
+    enddate = request.form['enddate']
+    return render_template('result.html', stock = stock, startdate = startdate, enddate = enddate)
 
 if __name__ == "__main__":
     app.run()
