d45328859b9394d172bbaba9259e66e20e238d4a
Anny Wei <anny.wei@student.tdsb.on.ca>
2021-09-19 01:05:00 -0400

    ok we try again

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import *
+from stockmarketpredictingapp import *
 #from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATA']
@@ -15,11 +16,21 @@ def index():
     return render_template('index.html')
 
 @app.route('/', methods = ['POST'])
-def get_stock():
+def getStock():
     stock = request.form['stock'] #id in html
     startdate = request.form['startdate']
     enddate = request.form['enddate']
-    return render_template('result.html', stock = stock, startdate = startdate, enddate = enddate)
+    return render_template('result.html', stock = stock)
+
+@app.route('/<name>', methods = ['POST', 'GET'])
+def getResult(name):
+    task.content = request.form['submit']
+    try:
+        return redirect('/')
+    except:
+        return 'There was an issue returning to the home page'
+
+
 
 if __name__ == "__main__":
     app.run()
